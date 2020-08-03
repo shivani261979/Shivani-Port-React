531b4f9413a3fcbce00d4f082eff7834083327c9
shivani jha <shivani261979@gmail.com>
2020-08-03 11:24:47 -0400

    Port in React

---
+++ src/components/introduction.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+// import useWindowSize from 'react-use/lib/useWindowSize'
+import Confetti from 'react-confetti'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight bgcolored">
+            <ul className="slides">
+              <li>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Hi! <br />I'm Shivani</h1>
+                          <p><a className="btn btn-primary btn-learn" href="/public/images/resume.pdf" target="_blank" rel="noopener noreferrer">View Resume<i className="icon-download4" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>I love building<br /> Applications!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://github.com/shivani261979" target="_blank" rel="noopener noreferrer">View Projects <i className="icon-briefcase3" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1> My Recommentations!</h1>
+                          <p><a className="btn btn-primary btn-learn" href="https://www.linkedin.com/in/shivani-jha26/" target="_blank" rel="noopener noreferrer">View at LinkedIn<i className="icon-book" /></a></p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div>
+            <Confetti
+            width="2000px"
+            height="1400px"
+            numberOfPieces="600"
+            tweenDuration="8"
+          />
+          </div>
+        </section>
+      </div>    
+    )
+  }
+}
---
+++ src/components/timeline.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>About Me! </h2>
+                        <p>12.5 years of experience successfully leading all technical aspects of enterprise level business intelligence and complex software initiatives from inception through project sign-off. Expertise includes coordinating teams as large as twenty developers, analysts, and testers to ensure timely completion of critical project deliverables. Several projects for which I provided leadership were highly customized solutions used to manage multi-billion dollar portfolios.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Professional Experience!</h2>
+                        <p>12.5 years of experience successfully leading all technical aspects of enterprise level business intelligence and complex software initiatives from inception through project sign-off. Expertise includes coordinating teams as large as twenty developers, analysts, and testers to ensure timely completion of critical project deliverables. Several projects for which I provided leadership were highly customized solutions used to manage multi-billion dollar portfolios.</p>
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2> Bootcamp Projects!</h2>
+                        <p>12.5 years of experience successfully leading all technical aspects of enterprise level business intelligence and complex software initiatives from inception through project sign-off. Expertise includes coordinating teams as large as twenty developers, analysts, and testers to ensure timely completion of critical project deliverables. Several projects for which I provided leadership were highly customized solutions used to manage multi-billion dollar portfolios.</p> 
+                      </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
